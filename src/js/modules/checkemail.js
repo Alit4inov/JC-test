740fd26dcb2524b1e14461ccf055e9bd2502cd80
Alit4inov <alit4inov@gmail.com>
2017-09-07 01:50:49 +0300

    add email validation

--- src/js/modules/checkemail.js
+++ src/js/modules/checkemail.js
@@ -1,8 +1,30 @@
 var checkEmail = function(email) {
 
-    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    if (!reg.test(email)) return false;
-    return true;
+   
+	var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
+	var mail = $('#mail');
+	var info = $('.validation-info');
+	
+	mail.blur(function(){
+		if(mail.val() != ''){
+				if(mail.val().search(pattern) == 0){
+					$('#valid').text('ОК');
+					$('#submit').attr('disabled', false);
+					mail.removeClass('error').addClass('ok');
+				}else{
+					$('#valid').text('Please enter valid email');
+					$('#submit').attr('disabled', true);
+					mail.addClass('ok');
+
+				}
+			}else{
+				$('#valid').text('Please enter valid email');
+				mail.addClass('error');
+				info.addClass('error');
+				$('#submit').attr('disabled', true);
+			}
+	});
+
 };
 
 export default checkEmail;
